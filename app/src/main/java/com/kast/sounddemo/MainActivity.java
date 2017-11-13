cf89714a86a0a012e550a03d4084a58ccededff4
Kaupo Aun <kaupo.aun@gmail.com>
2017-11-14 00:02:34 +0200

    Play and Pause buttons

--- app/src/main/java/com/kast/sounddemo/MainActivity.java
+++ app/src/main/java/com/kast/sounddemo/MainActivity.java
@@ -1,13 +1,32 @@
 package com.kast.sounddemo;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mplayer;
+
+    public void playButton(View view) {
+
+        mplayer.start();
+
+    }
+
+    public void pauseButton(View view) {
+
+        mplayer.pause();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mplayer = MediaPlayer.create(this, R.raw.shotgun);
+
     }
 }
